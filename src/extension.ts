6c4700944a112f874f73c01ca3c1f2568b2b2599
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-10-16 15:44:22 +0200

    init

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,67 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "test-ts" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	function timeout(ms: number) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	vscode.speech.registerSpeechProvider('sample', {
+		provideSpeechToTextSession: token => {
+			const emitter = new vscode.EventEmitter<vscode.SpeechToTextEvent>();
+
+			(async () => {
+				await timeout(1000);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Started });
+
+				await timeout(500);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Recognizing, text: 'Explain' });
+
+				await timeout(500);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Recognizing, text: 'Explain the' });
+
+				await timeout(500);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Recognizing, text: 'Explain the project' });
+
+				await timeout(500);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Recognizing, text: 'Explain the project to' });
+
+				await timeout(500);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Recognizing, text: 'Explain the project to me' });
+
+				await timeout(500);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Recognized, text: 'Explain the project to me.' });
+
+				await timeout(500);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Recognizing, text: 'And' });
+
+				await timeout(500);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Recognizing, text: 'And how' });
+
+				await timeout(500);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Recognizing, text: 'And how do' });
+
+				await timeout(500);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Recognizing, text: 'And how do you' });
+
+				await timeout(500);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Recognizing, text: 'And how do you debug' });
+
+				await timeout(500);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Recognized, text: 'And how do you debug?' });
+
+				await timeout(1000);
+				emitter.fire({ status: vscode.SpeechToTextStatus.Stopped });
+			})();
+
+			return {
+				onDidChange: emitter.event,
+				dispose: () => { }
+			}
+		}
 	});
-
-	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
